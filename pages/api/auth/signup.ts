8281858a73935719a324cb3700d27a6d13583f0e
m2vi <iuvo@gmx.at>
2021-11-19 22:04:48 +0100

    dd

--- pages/api/auth/signup.ts
+++ pages/api/auth/signup.ts
@@ -4,21 +4,18 @@ import type { NextApiRequest, NextApiResponse } from 'next';
 import pw from '@utils/password';
 import { core, discord } from '@utils/api';
 import { v4 } from 'uuid';
-import validator from 'validator';
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   const connection = await connectToDatabase();
   if (connection?.readyState !== 1) return res.status(500).json({ error: 'Database error' });
-  const { username, password, discord: discordId, mail } = req.query as any;
+  const { username, password, discord: discordId } = req.query as any;
 
-  if (!username || !password || !discordId || !mail) return res.status(400).json({ error: 'Missing params' });
-  if (!validator.isEmail(mail)) return res.status(400).json({ error: 'Email is not valid' });
+  if (!username || !password || !discordId) return res.status(400).json({ error: 'Missing params' });
   if (await core.exists(username)) return res.status(400).json({ error: 'Username is taken' });
   if (!(await discord.exists(discordId))) return res.status(400).json({ error: 'Discord id is not assigned' });
 
   const obj = new userSchema({
     uuid: v4(),
-    mail,
     username,
     password_hash: pw.hash(password),
     creation_date: Date.now(),
