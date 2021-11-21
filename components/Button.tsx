7bedf91175d7f18781b5e8e0f5fa89ed2942f636
Felix <felix.waidler@web.de>
2021-11-21 16:08:47 +0100

    auth

--- components/Button.tsx
+++ components/Button.tsx
@@ -9,7 +9,7 @@ const sizeClassnames = {
 const colorClassnames = {
   primary: 'text-button bg-accent hover:bg-accent-hover disabled:text-accent-disabled disabled:bg-accent-hover',
   secondary: 'text-button bg-primary-700 hover:bg-primary-600 disabled:text-primary-300',
-  'secondary-800': 'text-button bg-primaary-800 hover:bg-primary-600 disabled:text-primary-300',
+  'secondary-800': 'text-button bg-primary-800 hover:bg-primary-600 disabled:text-primary-300',
 };
 
 export type ButtonProps = DetailedHTMLProps<ButtonHTMLAttributes<HTMLButtonElement>, HTMLButtonElement> & {
--- pages/api/auth/signup.ts
+++ pages/api/auth/signup.ts
@@ -7,13 +7,15 @@ import { core, discord } from '@utils/api';
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   const connection = await connectToDatabase();
   if (connection?.readyState !== 1) return res.status(500).json({ error: 'Database error' });
-  const { username, password, discord: discordId } = req.query as any;
+  const { firstname, lastname, username, password, discord: discordId } = JSON.parse(req.body) as any;
 
   if (!username || !password || !discordId) return res.status(400).json({ error: 'Missing params' });
   if (await core.exists(username)) return res.status(400).json({ error: 'Username is taken' });
   if (!(await discord.exists(discordId))) return res.status(400).json({ error: 'Discord id is not assigned' });
 
   const obj = new userSchema({
+    firstname,
+    lastname,
     username,
     password_hash: pw.hash(password),
     creation_date: Date.now(),
