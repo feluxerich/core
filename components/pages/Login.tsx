f7b96d4d5f6f10e51af0949aca212760d3624e78
Felix <felix.waidler@web.de>
2021-11-28 16:01:37 +0100

    spelling error

--- components/pages/Login.tsx
+++ components/pages/Login.tsx
@@ -74,7 +74,7 @@ const Login = () => {
           Login
         </Button>
         <Link href="/signup" passHref>
-          <span className="mt-4 font-bold text-center cursor-pointer text-accent">Not a account yet? Create one.</span>
+          <span className="mt-4 font-bold text-center cursor-pointer text-accent">Not an account yet? Create one.</span>
         </Link>
       </div>
     </Full>
