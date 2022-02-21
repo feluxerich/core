0130a30523fc7f0ee8ae1ffc5e5b92c0897a635e
Felix <68397506+feluxerich@users.noreply.github.com>
2022-02-21 02:47:14 +0100

    middleware fix

--- pages/_middleware.ts
+++ pages/_middleware.ts
@@ -1,15 +1,10 @@
 import { NextRequest, NextResponse } from 'next/server';
 import config from '@config/middleware';
 import { verifyAuth } from '@utils/core/worker';
-import { vitals } from '@utils/vitals';
 
 export async function middleware(req: NextRequest) {
   if (!req.page.name || !config.restricted.includes(req.page.name)) return;
   const [verified, error, jwt] = await verifyAuth(req);
-
-  const report = vitals.worker(req);
-
-  console.log(report);
 
   if (error) {
     // error
