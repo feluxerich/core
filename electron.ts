6dac6292131d1bcf8b4402cf2bd9ace29d29c2d2
Felix <68397506+feluxerich@users.noreply.github.com>
2022-02-26 14:43:19 +0100

    added electron

---
+++ electron.ts
@@ -0,0 +1,33 @@
+const { app, BrowserWindow, dialog } = require('electron');
+
+const createWindow = () => {
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    fullscreen: true,
+    closable: true,
+    autoHideMenuBar: true,
+  });
+
+  win.webContents.on('before-input-event', (event, input) => {
+    if (input.key.toLowerCase() === 'escape') {
+      var resp = dialog.showMessageBoxSync(win, {
+        buttons: ['&Yes', '&No'],
+        message: 'Do you want to quit?',
+        noLink: true,
+      });
+      if (resp === 0) app.quit();
+      event.preventDefault();
+    }
+  });
+
+  win.loadURL('https://fluxi.me/');
+};
+
+app.whenReady().then(() => {
+  createWindow();
+});
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') app.quit();
+});
