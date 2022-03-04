2a0758e801d08b545ffc7d1adcf2137650ff5361
Felix <68397506+feluxerich@users.noreply.github.com>
2022-03-04 05:03:17 +0100

    removed covid stats

--- pages/api/covid/germany/now.ts
+++
@@ -1,8 +0,0 @@
-import { basicFetch } from '@utils/fetch';
-import type { NextApiRequest, NextApiResponse } from 'next';
-
-export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-  const germany = await basicFetch('https://api.corona-zahlen.org/germany');
-
-  res.status(200).json(germany);
-}
