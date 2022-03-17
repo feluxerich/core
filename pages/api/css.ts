73626365f6acf9bb6f81f33928cab22fa784f379
m2vi <iuvo@gmx.at>
2022-03-17 12:17:37 +0100

    added css thing

---
+++ pages/api/css.ts
@@ -0,0 +1,23 @@
+import { baseUrl, basicFetch } from '@m2vi/iva';
+import type { NextApiRequest, NextApiResponse } from 'next';
+import cache from 'memory-cache';
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+  const reset = new Object(req.query).hasOwnProperty('reset');
+
+  if (!reset && cache.get('css')) {
+    res.setHeader('content-type', 'text/css');
+    res.send(cache.get('css'));
+    return;
+  }
+
+  const b = baseUrl(req);
+  const files = ['/styles/globals.css', '/styles/colors.css', '/styles/components.css', '/styles/animations.css'];
+
+  const data = (await Promise.all(files.map(async route => await basicFetch(`${b}${route}`, 'text')))).join('\n');
+
+  cache.put('css', data);
+
+  res.setHeader('content-type', 'text/css');
+  res.send(data);
+}
