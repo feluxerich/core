a4cbd3b397bdb67dab6b36f5ef05d4ca7930b4a3
m2vi <iuvo@gmx.at>
2022-05-11 21:40:14 +0200

    fix css

--- pages/api/css.ts
+++ pages/api/css.ts
@@ -3,23 +3,27 @@ import type { NextApiRequest, NextApiResponse } from 'next';
 import cache from 'memory-cache';
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-  const reset = new Object(req.query).hasOwnProperty('reset');
+  try {
+    const reset = new Object(req.query).hasOwnProperty('reset');
 
-  if (!reset && cache.get('css')) {
+    if (!reset && cache.get('css')) {
+      res.setHeader('access-control-allow-origin', '*');
+      res.setHeader('content-type', 'text/css');
+      res.send(cache.get('css'));
+      return;
+    }
+
+    const b = baseUrl(req);
+    const files = ['/styles/globals.css', '/styles/colors.css', '/styles/components.css', '/styles/animations.css'];
+
+    const data = (await Promise.all(files.map(async route => await basicFetch(`${b}${route}`, 'text')))).join('\n');
+
+    cache.put('css', data);
+
     res.setHeader('access-control-allow-origin', '*');
     res.setHeader('content-type', 'text/css');
-    res.send(cache.get('css'));
-    return;
+    res.send(data);
+  } catch (error: any) {
+    res.status(500).json({ error: error?.message });
   }
-
-  const b = baseUrl(req);
-  const files = ['/styles/globals.css', '/styles/colors.css', '/styles/components.css', '/styles/animations.css'];
-
-  const data = (await Promise.all(files.map(async route => await basicFetch(`${b}${route}`, 'text')))).join('\n');
-
-  cache.put('css', data);
-
-  res.setHeader('access-control-allow-origin', '*');
-  res.setHeader('content-type', 'text/css');
-  res.send(data);
 }
